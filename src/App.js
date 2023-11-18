c407b41cad0a9ef983548b4e84683b1faa9ec698
Vladimir Kratinov <vladimirkratinov@gmail.com>
2023-11-18 14:28:46 -0800

    refactor Card Element  using map() method

--- src/App.js
+++ src/App.js
@@ -3,48 +3,29 @@ import "./style.css";
 import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
 import Card from "./components/Card";
+import data from "./data";
 
 function App() {
+  const cards = data.map(object => {
+    return <Card
+    key={object.id} 
+    image={object.image}
+    title={object.title}
+    rating={object.stats.rating}
+    reviewCount={object.stats.reviewCount}
+    duration={object.duration}
+    price={object.price}
+    />
+  })
+
+  console.log(data)
+
   return (
     <div>
       <NavBar />
       <Hero />
       <div className="grid-container">
-        <Card
-          image="card-1.jpeg"
-          title="Portrait Photography in Vancouver"
-          rating="4.88"
-          reviewCount={50}
-          duration="1 hour"
-          price={95}
-        />
-
-        <Card
-          image="card-2.jpeg"
-          title="Night photography with a pro"
-          rating="5.0"
-          reviewCount={6}
-          duration="2 hours"
-          price={76}
-        />
-
-        <Card
-          image="card-3.jpeg"
-          title="Craft beer tour & tastings on a street art walk - Vancouver"
-          rating="4.91"
-          reviewCount={246}
-          duration="3 hours"
-          price={70}
-        />
-
-        <Card
-          image="card-4.jpeg"
-          title="Vancouver R&B Sounds"
-          rating="4.89"
-          reviewCount={38}
-          duration="2 hours"
-          price={28}
-        />
+        {cards}
       </div>
     </div>
   );
---
+++ src/data.js
@@ -0,0 +1,56 @@
+const data = [
+  {
+    id: 1,
+    image: "card-1.jpeg",
+    title: "Portrait Photography in Vancouver",
+    price: 95,
+    stats: {
+      rating: 5.0,
+      reviewCount: 50,
+    },
+    duration: "1 hour",
+    location: "Online",
+    openSpots: 0
+  },
+  {
+    id: 2,
+    image: "card-2.jpeg",
+    title: "Night photography with a pro",
+    price: 76,
+    stats: {
+      rating: 5.0,
+      reviewCount: 6,
+    },
+    duration: "2 hours",
+    location: "Online",
+    openSpots: 12
+  },
+  {
+    id: 3,
+    image: "card-3.jpeg",
+    title: "Craft beer tour & tastings on a street art walk - Vancouver",
+    price: 70,
+    stats: {
+      rating: 4.91,
+      reviewCount: 246,
+    },
+    duration: "3 hours",
+    location: "Online",
+    openSpots: 24
+  },
+  {
+    id: 4,
+    image: "card-4.jpeg",
+    title: "Vancouver R&B Sounds",
+    price: 28,
+    stats: {
+      rating: 4.89,
+      reviewCount: 38,
+    },
+    duration: "2 hours",
+    location: "Online",
+    openSpots: 17
+  },
+];
+
+export default data;
